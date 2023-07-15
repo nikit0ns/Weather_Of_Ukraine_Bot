47a689164c459afcfd3cbaa4b6f9fc1ef2506715
Nikita Kasyanenko <nikitakasyanenko2005@gmail.com>
2023-07-15 23:13:29 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+TOKEN = '...' #Сюда вы пишите свой токен. Узнать его можно в @BotFather 
+Open_Weather_Token = '76eab5554dd59c020272ba11b09cd78d' #Узнать токен можно на сайте https://openweathermap.org/api
